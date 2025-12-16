80ccc3b11ed654752b08ca5eaca05d65f07a6d7b
Zuzia Dybcio <moriendi1422@gmail.com>
2025-12-16 15:19:50 +0100

    Initial commit

---
+++ Platforma.py
@@ -0,0 +1,21 @@
+import pygame
+
+WYSOKOSC_EKRANU = 800 #400
+SZEROKOSC_EKRANU = 1024 #512
+
+class Platforma(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Platforma, self).__init__()
+        self.obraz = pygame.image.load("images/pad.png")
+        self.porusza_sie = 0
+        self.zresetuj_pozycje()
+
+    def zresetuj_pozycje(self):
+        self.rect = pygame.Rect(SZEROKOSC_EKRANU/2-70, WYSOKOSC_EKRANU*0.85, 140, 30)
+
+    def ruszaj_platforma(self, wartosc): 
+        predkosc = 10 
+        self.porusza_sie = wartosc
+        self.rect.move_ip(wartosc*predkosc, 0) 
+        if self.rect.left <= 0: self.rect.x = 0 
+        if self.rect.right >= SZEROKOSC_EKRANU: self.rect.x = SZEROKOSC_EKRANU-140 
---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from Platforma import Platforma
+
+WYSOKOSC_EKRANU = 800 #400
+SZEROKOSC_EKRANU = 1024 #512
+
+ekran = pygame.display.set_mode([SZEROKOSC_EKRANU, WYSOKOSC_EKRANU])
+zegar = pygame.time.Clock()
+obraz_tla = pygame.image.load("images/background.png")
+
+platforma = Platforma()
+
+gra_dziala = True 
+while gra_dziala: 
+    for zdarzenie in pygame.event.get(): 
+        if zdarzenie.type == pygame.KEYDOWN: 
+            if zdarzenie.key == pygame.K_ESCAPE: 
+                gra_dziala = False 
+        elif zdarzenie.type == pygame.QUIT: 
+            gra_dziala = False 
+
+
+    keys = pygame.key.get_pressed() 
+    if keys[pygame.K_LEFT]: 
+        platforma.ruszaj_platforma(-1) 
+    if keys[pygame.K_RIGHT]: 
+        platforma.ruszaj_platforma(1) 
+
+    ekran.blit(obraz_tla, (0,0)) 
+    ekran.blit(platforma.obraz, platforma.rect)
+
+    pygame.display.flip() 
+    zegar.tick(30) 
+pygame.quit()
